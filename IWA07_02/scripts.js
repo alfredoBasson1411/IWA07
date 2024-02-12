41cd94c7dad7ce0eb0fc51f8b7a3ccafb61126a5
alfredoBasson1411 <alfredojemainebasson14@gmail.com>
2024-02-12 13:57:16 +0200

    compelted IWA07

---
+++ IWA07_02/scripts.js
@@ -0,0 +1,16 @@
+const nickname = "Timmy";
+const firstname = "Timothy";
+
+if (nickname) {
+    console.log(`Good morning, ${nickname}!`);
+} else if (firstname) {
+    console.log(`Good morning, ${firstname}!`);
+} else {
+    console.log("Good morning!");
+}
+//the original code used single quotes to enclose the string which does not allow for string interpolation.
+//it required backticks in JS
+//the variables wasnt properly interpolated
+// so i used the '${}' syntax inside a template literal
+//the logical OR Operator was treated as a string literal not as L.O.O
+//L.O.O is used for Boolean logic.
---
+++ IWA07_03/scripts.js
@@ -0,0 +1,33 @@
+const leoName = 'Leo';
+const leoSurname = 'Musvaire     ';
+const leoBalance = '-9394';
+
+const sarahName = 'Sarah    ';
+const sarahSurname = 'Kleinhans';
+const sarahBalance = '-4582.2';
+
+const divider = '----------------------------------';
+
+// Only change below this line
+
+// Convert leoBalance and sarahBalance to numbers using parseFloat
+const leoBalanceNumber = parseFloat(leoBalance);
+const sarahBalanceNumber = parseFloat(sarahBalance);
+
+// Calculate the total owed amount
+const totalOwed = Math.abs(leoBalanceNumber) + Math.abs(sarahBalanceNumber);
+
+// Format the total owed amount to have 2 decimal places and include thousand separators
+const formattedTotalOwed = totalOwed.toLocaleString('en-ZA', { style: 'currency', currency: 'ZAR' });
+
+// Create the strings for Leo and Sarah's owed amounts
+const leo = `${leoName} ${leoSurname.trim()} (Owed: R ${Math.abs(leoBalanceNumber).toFixed(2)})`;
+const sarah = `${sarahName} ${sarahSurname.trim()} (Owed: R ${Math.abs(sarahBalanceNumber).toFixed(2)})`;
+
+// Create the final output
+const result = `\n${leo}\n${sarah}\n${divider}\n${divider}\n  Total amount owed: ${formattedTotalOwed}\n${divider}`;
+
+console.log(result);
+//i used 'Math.abs()' around 'leoBalanceNumber and 'sarhaBalanceNumber'
+//when constructing the strings for leo and sarah's owed amounts
+//this ensures that the displayed amounts are positive.
